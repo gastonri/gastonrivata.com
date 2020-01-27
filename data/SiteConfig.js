ecb0422e5d3ab81ccbc46a590f4a78ec16f56644
Gastón Rivata <gastonrivata@gmail.com>
2020-01-27 08:09:21 -0300

    Remove file and folder and refactor functions for readability

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,7 +1,7 @@
 const config = {
-  siteTitle: "Gatsby Advanced Starter", // Site title.
-  siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
+  siteTitle: "GastónRivata.com", // Site title.
+  siteTitleShort: "GastónRivata", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "GastónRivata.com", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
@@ -14,10 +14,10 @@ const config = {
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   postsPerPage: 4, // Amount of posts displayed per listing page.
-  userName: "Advanced User", // Username to display in the author segment.
-  userEmail: "AdvancedUser@example.com", // Email used for RSS feed's author segment
+  userName: "Gastón Rivata", // Username to display in the author segment.
+  userEmail: "gastonrivata@gmail.com", // Email used for RSS feed's author segment
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userLocation: "Córdoba, Argentina", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
--- src/templates/listing.jsx
+++ src/templates/listing.jsx
@@ -1,83 +1,126 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql, Link } from "gatsby";
-import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
-import SEO from "../components/SEO/SEO";
-import config from "../../data/SiteConfig";
-import "./listing.css";
+import './listing.css';
+import config from '../../data/SiteConfig';
+import Helmet from 'react-helmet';
+import Layout from '../layout';
+import PostListing from '../components/PostListing/PostListing';
+import SEO from '../components/SEO/SEO';
+import React from 'react';
+import { graphql, Link } from 'gatsby';
 
 class Listing extends React.Component {
-  renderPaging() {
-    const { currentPageNum, pageCount } = this.props.pageContext;
-    const prevPage = currentPageNum - 1 === 1 ? "/" : `/${currentPageNum - 1}/`;
-    const nextPage = `/${currentPageNum + 1}/`;
-    const isFirstPage = currentPageNum === 1;
-    const isLastPage = currentPageNum === pageCount;
+	render() {
+		return (
+			<Layout>
+				<div className="listing-container">
+					{this.renderPostsContainer()}
+					{this.renderPaging()}
+				</div>
+			</Layout>
+		);
+	}
 
-    return (
-      <div className="paging-container">
-        {!isFirstPage && <Link to={prevPage}>Previous</Link>}
-        {[...Array(pageCount)].map((_val, index) => {
-          const pageNum = index + 1;
-          return (
-            <Link
-              key={`listing-page-${pageNum}`}
-              to={pageNum === 1 ? "/" : `/${pageNum}/`}
-            >
-              {pageNum}
-            </Link>
-          );
-        })}
-        {!isLastPage && <Link to={nextPage}>Next</Link>}
-      </div>
-    );
-  }
+	renderPostsContainer() {
+		return (
+			<div className="posts-container">
+				{this.renderHelmet()}
+				<SEO />
+				{this.renderPostListing()}
+			</div>
+		);
+	}
 
-  render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
+	renderHelmet() {
+		return <Helmet title={config.siteTitle} />;
+	}
 
-    return (
-      <Layout>
-        <div className="listing-container">
-          <div className="posts-container">
-            <Helmet title={config.siteTitle} />
-            <SEO />
-            <PostListing postEdges={postEdges} />
-          </div>
-          {this.renderPaging()}
-        </div>
-      </Layout>
-    );
-  }
+	renderPostListing() {
+		const postEdges = this.props.data.allMarkdownRemark.edges;
+
+		return <PostListing postEdges={postEdges} />;
+	}
+
+	renderPaging() {
+		const { currentPageNum, pageCount } = this.props.pageContext;
+		const prevPage =
+			currentPageNum - 1 === 1 ? '/' : `/${currentPageNum - 1}/`;
+		const nextPage = `/${currentPageNum + 1}/`;
+		const isFirstPage = currentPageNum === 1;
+		const isLastPage = currentPageNum === pageCount;
+
+		return (
+			<div className="paging-container">
+				{this.renderPreviousLink(isFirstPage, prevPage)}
+				{this.renderLinkList()}
+				{this.renderNextLink(isLastPage, nextPage)}
+			</div>
+		);
+	}
+
+	renderPreviousLink(isFirstPage, prevPage) {
+		let dataToRender = null;
+
+		if (!isFirstPage) {
+			dataToRender = <Link to={prevPage}>Previous</Link>;
+		}
+
+		return dataToRender;
+	}
+
+	renderLinkList() {
+		const { pageCount } = this.props.pageContext;
+
+		return [...Array(pageCount)].map((_val, index) => {
+			const pageNum = index + 1;
+			return <Link {...this.getLinkProps(pageNum)}>{pageNum}</Link>;
+		});
+	}
+
+	renderNextLink(isLastPage, nextPage) {
+		let dataToRender = null;
+
+		if (!isLastPage) {
+			dataToRender = <Link to={nextPage}>Next</Link>;
+		}
+
+		return dataToRender;
+	}
+
+	getLinkProps(pageNum) {
+		let charToRender = pageNum === 1 ? '/' : `/${pageNum}/`;
+
+		return {
+			key: `listing-page-${pageNum}`,
+			to: charToRender
+		};
+	}
 }
 
 export default Listing;
 
 /* eslint no-undef: "off" */
 export const listingQuery = graphql`
-  query ListingQuery($skip: Int!, $limit: Int!) {
-    allMarkdownRemark(
-      sort: { fields: [fields___date], order: DESC }
-      limit: $limit
-      skip: $skip
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
+	query ListingQuery($skip: Int!, $limit: Int!) {
+		allMarkdownRemark(
+			sort: { fields: [fields___date], order: DESC }
+			limit: $limit
+			skip: $skip
+		) {
+			edges {
+				node {
+					fields {
+						slug
+						date
+					}
+					excerpt
+					timeToRead
+					frontmatter {
+						title
+						tags
+						cover
+						date
+					}
+				}
+			}
+		}
+	}
 `;
